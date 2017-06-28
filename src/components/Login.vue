2aa7c3da3960388e599d8a722362d9e1014eb11a
Daniel Villaça <danielvillacasilva@gmail.com>
2017-06-28 16:15:54 -0300

    Just trying out the flow inside the scaffold
    + Added a login component and route to understand how things work, the monster is ready to break its jail

---
+++ src/components/Login.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="login">
+        <h1>{{ msg }}</h1>
+        <form action="www.google.com" method="POST">
+            <input type="text"/>
+            <input type="submit" value="Logar"/>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: 'Facebook login'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h1, h2 {
+        font-weight: normal;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+</style>
